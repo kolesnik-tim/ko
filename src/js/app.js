3c6d5ad6df98966e7f5b11947ffda2dd468573b8
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-02-27 14:25:36 +0200

    page

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,33 @@
 // import sayHello from './lib/sayHello.js';
+import mask from 'jquery.maskedinput';
+import jquery from 'jquery';
+import rangeslider from 'rangeslider.js';
 
+
+
+
+//pop-up
+$('.icon-menu-icon').on('click', function() {
+  $('.menu').addClass('active');
+});
+$('.btn--bid-1').on('click', function() {
+  $('.bid-1').addClass('active');
+});
+$('.btn--bid-2').on('click', function() {
+  $('.bid-2').addClass('active');
+});
+$('.btn--consultant').on('click', function() {
+  $('.consultant').addClass('active');
+});
+
+$('.close').on('click', function() {
+  $('.pop-up').removeClass('active');
+});
+
+// //mask
+// jQuery(function($) {
+//   $('#phone-1').mask('(999) 999-9999');
+// });
+
+//range
+$('input[type="range"]').rangeslider();
