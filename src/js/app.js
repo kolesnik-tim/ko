1f54b40d2bf838f3b2011db7171347ef9ba715f8
Timothy <31729676+kolesnik-tim@users.noreply.github.com>
2018-02-27 23:29:15 +0200

    fixed colculator

--- src/js/app.js
+++ src/js/app.js
@@ -1,41 +1,74 @@
-import Jquery from 'jquery';
+
 import './lib/maskedinput';
 import rangeslider from 'rangeslider.js';
-import ScrollMagic from 'scrollmagic';
+// import ScrollMagic from 'scrollmagic';
 
 
 
+//калькулятор
+let coins = $('.coins');
+let credit = $('.credit');
+let percent = $('.percent');
+let sum = $('.sum');
+
+coins.change(function() {
+  computation();
+  $('.coins__res').text(coins.val());
+});
+credit.change(function() {
+  computation();
+  $('.credit__res').text(credit.val());
+  console.log(+credit.val());
+});
+percent.change(function() {
+  computation();
+  $('.percent__res').text(+percent.val(), '%');
+});
+
+function computation() {
+  let res = Math.pow(percent.val(), credit.val());
+  let division = (percent.val() * res)/(res - 1);
+  let amount = coins.val() * division;
+  sum.text(res);
+}
 
 
 //pop-up
+
+// menu
 $('.icon-menu-icon').on('click', function() {
   $('.menu').addClass('active');
 });
+// pop-up 1
 $('.btn--bid-1').on('click', function() {
   $('.bid-1').addClass('active');
 });
+//pop-up 2
 $('.btn--bid-2').on('click', function() {
   $('.bid-2').addClass('active');
 });
+// pop-up 3
 $('.btn--consultant').on('click', function() {
   $('.consultant').addClass('active');
 });
-
+//pop-up close
 $('.close').on('click', function() {
   $('.pop-up').removeClass('active');
 });
+
 
 //mask
 jQuery(function($) {
   $('input[type="tel"]').mask('+7(699) 999-9999');
 });
 
+
 //range
 $('input[type="range"]').rangeslider();
 
 
 
-
+//logo
 $(window).scroll(function() {
   var scrolled = $(this).scrollTop();
   if( scrolled >= 10 && ! $('.header').hasClass('sticked') ) {
