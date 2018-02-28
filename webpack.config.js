8d56be16a286a63cbce706490d8bde2422546701
kabyr <eugenepro@icloud.com>
2018-02-28 12:59:43 +0200

    calculator

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ function createConfig(env) {
   webpackConfig = {
     context: path.join(__dirname, config.src.js),
     entry: {
-      // vendor: ['jquery'],
+      vendor: ['jquery'],
       app: './app.js',
     },
     output: {
